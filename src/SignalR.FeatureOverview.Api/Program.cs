bfe764a237922d797952aef9be519885e409fd3e
Aleksei Mialkin <aleksei@mialkin.ru>
2024-03-10 16:26:37 +0300

    Implement basic SignalR example

--- src/SignalR.FeatureOverview.Api/Program.cs
+++ src/SignalR.FeatureOverview.Api/Program.cs
@@ -1,6 +1,15 @@
+using SignalR.FeatureOverview.Api;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+
 var application = builder.Build();
 
+application.UseStaticFiles();
+application.UseRouting();
+
 application.MapGet("/", () => "Hello World!");
+
+application.MapHub<ChatHub>("/chatHub");
 
 application.Run();
